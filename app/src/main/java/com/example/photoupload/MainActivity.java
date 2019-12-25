070773a7d78484db67f1e5e09e8233fe3c613b65
Lad-Rahul <rahullad945@gmail.com>
2019-12-25 13:36:13 +0530

    take image

--- app/src/main/java/com/example/photoupload/MainActivity.java
+++ app/src/main/java/com/example/photoupload/MainActivity.java
@@ -3,19 +3,27 @@ package com.example.photoupload;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.Random;
 
@@ -24,16 +32,64 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final int PICK_IMAGE_REQUEST = 1;
 
+    private Button mButtonChooseImage;
+    private TextView mTextViewShowUploads;
+    private EditText mEditTextFileName;
+    private ImageView mImageView;
+    private ProgressBar mProgressbar;
+    private Button mButtonUpload;
+    private Uri mImageUri;
 
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButtonChooseImage = (Button)findViewById(R.id.btn_choose_image);
+        mTextViewShowUploads = (TextView)findViewById(R.id.text_view_show_upload);
+        mEditTextFileName = (EditText)findViewById(R.id.edittext_img_name);
+        mImageView = (ImageView)findViewById(R.id.image_view);
+        mProgressbar = (ProgressBar)findViewById(R.id.progress_bar);
+        mButtonUpload = (Button)findViewById(R.id.btn_upload);
 
+        mStorageRef = FirebaseStorage.getInstance().getReference("img_uploads");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("img_uploads");
+
+
+        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+
+
+    }//OnCreate
+
+    private void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+
+            mImageUri = data.getData();
+
+            Picasso.with(this).load(mImageUri).into(mImageView);
+
+        }
+
+    }
 }
